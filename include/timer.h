ff7cb119ded47d68bd9b1d78304418dafd378b62
helloKM <jimi_z@163.com>
2015-07-23 20:07:03 +0800

    changes log

---
+++ include/timer.h
@@ -0,0 +1,24 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  timer.h
+ *
+ *    Description:  定义 PIT(周期中断定时器) 相关函数
+ *
+ *        Version:  1.0
+ *        Created:  2015年7月23日 11时26分04秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *
+ * =====================================================================================
+ */
+
+#ifndef INCLUDE_TIMER_H_
+#define INCLUDE_TIMER_H_
+
+#include "types.h"
+
+void init_timer(uint32_t frequency);
+
+#endif 	// INCLUDE_TIMER_H_
---
+++ include/types.h
@@ -0,0 +1,36 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  types.h
+ *
+ *    Description:  一些类型的定义
+ *
+ *        Version:  1.0
+ *        Created:  2015年7月23日 16时28分07秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *
+ * =====================================================================================
+ */
+
+#ifndef INCLUDE_TYPES_H_
+#define INCLUDE_TYPES_H_
+
+#ifndef NULL
+	#define NULL 0
+#endif
+
+#ifndef TRUE
+	#define TRUE  1
+	#define FALSE 0
+#endif
+
+typedef unsigned int   uint32_t;
+typedef          int   int32_t;
+typedef unsigned short uint16_t;
+typedef          short int16_t;
+typedef unsigned char  uint8_t;
+typedef          char  int8_t;
+
+#endif 	// INCLUDE_TYPES_H_
